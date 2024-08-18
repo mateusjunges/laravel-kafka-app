a0ad0ee61796a3f8817edb55b58466fa464be98e
Mateus Junges <mateus@junges.dev>
2024-08-18 19:17:28 -0300

    Add command

---
+++ app/Console/Commands/Producer.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Junges\Kafka\Facades\Kafka;
+
+final class Producer extends Command
+{
+    /** @var string */
+    protected $signature = 'produce';
+
+    /** @var string */
+    protected $description = 'Produces a new message to Kafka.';
+
+    public function handle(): int
+    {
+        Kafka::publish('localhost:9092')
+            ->onTopic('my_messages')
+            ->withBodyKey('foo', 'bar')
+            ->withBodyKey('source', 'http request')
+            ->send();
+
+        return Command::SUCCESS;
+    }
+}
